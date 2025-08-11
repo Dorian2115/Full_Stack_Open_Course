e540467c6c22165e2a7da379f0eca46456d433bf
Dominik Długołencki <145024772+Dorian2115@users.noreply.github.com>
2025-08-11 16:25:00 +0000

    unicafe step 4

--- Part 1/unicafe/src/App.jsx
+++ Part 1/unicafe/src/App.jsx
@@ -7,7 +7,6 @@ const Statistics = (props) => {
 
   return (
     <>
-      <h1>statistics</h1>
       <p>good {props.good}</p>
       <p>neutral {props.neutral}</p>
       <p>bad {props.bad}</p>
@@ -26,9 +25,11 @@ const App = () => {
 
   return (
     <div>
+      <h1>give feedback</h1>
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
