0b7e724fd46dcf5b88fd4999f94ba9bf490964c9
Dominik Długołencki <145024772+Dorian2115@users.noreply.github.com>
2025-09-16 13:03:01 +0000

    Blog List, step 1

---
+++ Part 4/blog_list/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+const mongoUrl = process.env.MONGODB_URI;
+mongoose.connect(mongoUrl);
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  response.send("<h1>Welcome to Blog List API!</h1>");
+});
+
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+app.post("/api/blogs", (request, response) => {
+  const body = request.body;
+
+  if (!body.title || !body.url) {
+    return response.status(400).json({ error: "title or url missing" });
+  }
+
+  const blog = new Blog(body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
