3e3e23148bd108d249afd83256cd9144c755d039
Dominik Długołencki <145024772+Dorian2115@users.noreply.github.com>
2025-09-16 18:04:04 +0000

    Blog List, step 2

---
+++ Part 4/blog_list/controllers/blogs.js
@@ -0,0 +1,24 @@
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
+
+blogsRouter.get("/", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+blogsRouter.post("/", (request, response) => {
+  const body = request.body;
+
+  if (!body.title || !body.url) {
+    return response.status(400).json({ error: "title or url missing" });
+  }
+
+  const blog = new Blog(body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+module.exports = blogsRouter;
---
+++ Part 4/blog_list/utils/middleware.js
@@ -0,0 +1,1 @@
+//chyba zostawiam pusty narazie
