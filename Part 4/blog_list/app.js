a66a503d1d840ac2fc2726f6a601bbf6418225e9
Dominik Długołencki <145024772+Dorian2115@users.noreply.github.com>
2025-10-16 15:04:51 +0000

    Blog List Expansion, step 6

--- Part 4/blog_list/app.js
+++ Part 4/blog_list/app.js
@@ -4,6 +4,7 @@ const config = require("./utils/config");
 const logger = require("./utils/logger");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
 const app = express();
 
@@ -19,6 +20,7 @@ mongoose
   });
 
 app.use(express.json());
+app.use("/api/login", loginRouter);
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 
