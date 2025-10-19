58f6083a82720c2991a2237334b75b4d17135d5d
Dominik Długołencki <145024772+Dorian2115@users.noreply.github.com>
2025-10-19 10:12:07 +0000

    4.19: Blog List Expansion, step 7

--- Part 4/blog_list/controllers/blogs.js
+++ Part 4/blog_list/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
 
 blogsRouter.get("/", async (request, response) => {
   const responseBlogs = await Blog.find({}).populate("user", {
@@ -21,8 +30,13 @@ blogsRouter.get("/:id", async (request, response) => {
 
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
 
-  const user = await User.findById(body.userId);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
+
+  const user = await User.findById(decodedToken.id);
 
   if (!user) {
     return response.status(400).json({ error: "userId missing or not valid" });
