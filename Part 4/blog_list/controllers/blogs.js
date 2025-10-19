0bbb583b7b90c2bdedca4b11af3a72237a2544bd
Dominik Długołencki <145024772+Dorian2115@users.noreply.github.com>
2025-10-19 10:43:33 +0000

    Blog List Expansion, step 9

--- Part 4/blog_list/controllers/blogs.js
+++ Part 4/blog_list/controllers/blogs.js
@@ -54,8 +54,23 @@ blogsRouter.post("/", async (request, response) => {
 });
 
 blogsRouter.delete("/:id", async (request, response) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
+
+  const user = await User.findById(decodedToken.id);
+  const blog = await Blog.findById(request.params.id);
+
+  if (blog.user.toString() !== user._id.toString()) {
+    return response
+      .status(401)
+      .json({ error: "only the creator can delete a blog" });
+  }
+
   //findByIdAndRemove is deprecated - use findByIdAndDelete instead !!!!!!!!!!!!
-  await Blog.findByIdAndDelete(request.params.id);
+  await Blog.deleteOne({ _id: request.params.id });
   response.status(204).end();
 });
 
